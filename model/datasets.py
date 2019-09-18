ff48eeb4f331a9ffcfa0ff7a651c4dcb856bcb42
Jon Shamir <jon.shamir@gmail.com>
2019-09-18 19:32:13 +0300

    Fixing bugs

--- model/datasets.py
+++ model/datasets.py
@@ -73,7 +73,7 @@ class RandomPatchDataset(Dataset):
         h = np.random.randint(self.image_size[0]//4,self.image_size[0]//2)
         w = np.random.randint(self.image_size[1]//4,self.image_size[1]//2)
         mask[max(0,x-h//2):min(self.image_size[0],x+h//2),max(0,y-w//2):min(self.image_size[1],y+w//2)] = 0
-        target_image = original_image.copy()
+        target_image = original_image.clone()
         target_image[0][1-mask > 0.5] = np.max(target_image)
 
         mask = mask.reshape((1,)+mask.shape)
